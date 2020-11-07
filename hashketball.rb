aa8234a6ec06628cc19cd211aa53806e55e964ff
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-11-07 01:22:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -219,6 +219,7 @@ def big_shoe_rebounds
         value.each do |player_hash|
           if player_hash[:shoe] > largest_shoe_size
             largest_shoe_size = player_hash[:shoe]
+            binding.pry
           end
         end
       end
